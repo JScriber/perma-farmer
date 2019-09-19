9d2dc29b319f2b76af9b98c400929ef3df59b81e
jdiot <diot.jeremy@gmail.com>
2019-09-19 09:58:11 +0200

    fix add delete and modifier issue in stock block

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -24,29 +24,28 @@ Class StockController extends Controller{
 
         $product = Product::where("id", $request->id)->first();
 
-        // var_dump($product);
-
-
         return view('stock.edit')->with('product',$product);
-
-        // var_dump($request->id);
     }
 
 
     public function validEdit(Request $request){
 
+
         $data = array("name" => $request->nom,"weight" => $request->poids, "quantity"=> $request->quantite);
 
         Product::where("id", $request->id)->update($data);
 
-        return view('stock.show')->with('stock',Product::all()->all());
+
+        $request->user()->authorizeRole(Role::adminRole());
+        $product =Product::latest()->paginate(10);
+        return view('stock.show',[
+            "stock"=>$product
+        ])->with('i', (request()->input('page', 1) - 1) * 10);
     }
 
 
     public function add(Request $request){
         return view('stock.add');
-
-        // var_dump($request);
     }
 
     public function validAdd(Request $request){
@@ -55,18 +54,22 @@ Class StockController extends Controller{
 
         Product::create($data);
 
-        return view('stock.show')->with('stock',Product::all()->all());
+        $request->user()->authorizeRole(Role::adminRole());
+        $product =Product::latest()->paginate(10);
+        return view('stock.show',[
+            "stock"=>$product
+        ])->with('i', (request()->input('page', 1) - 1) * 10);
     }
 
     public function delete(Request $request){
 
         Product::where("id","=",$request->id)->delete();
-
-        return view('stock.show')->with('stock',Product::all()->all());
-
-        // var_dump($request->id);
-
-        // var_dump($request);
+        
+        $request->user()->authorizeRole(Role::adminRole());
+        $product =Product::latest()->paginate(10);
+        return view('stock.show',[
+            "stock"=>$product
+        ])->with('i', (request()->input('page', 1) - 1) * 10);
     }
 }
 
